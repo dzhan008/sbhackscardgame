5e0ce49c3243f935b1a539520f9e456fcefffdc6
danyo3 <ddiep003@ucr.edu>
2015-01-30 22:25:36 -0800

    Recruit class
    No Idea what to do for the functions yet

---
+++ sbhackscardgame/Card.h
@@ -0,0 +1,17 @@
+#ifndef CARD_H_INCLUDED
+#define CARD_H_INCLUDED
+
+class Recruit()
+{
+public:
+    Recruit();
+    Recruit( unsigned x);
+private:
+    int Health;
+    int Atk;
+    int Cost;
+
+
+};
+
+#endif // CARD_H_INCLUDED
